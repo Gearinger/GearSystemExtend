801c005828db0fbad59950754356c18b69d9eace
guoyd <guoyd@southzn.com>
2020-05-26 17:17:20 +0800

    1

--- GearSystemExtend/GearJson.cs
+++ GearSystemExtend/GearJson.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GearSystemExtend
 {
-    public class Class1
+    class GearJson
     {
     }
 }
---
+++ GearSystemExtend/StringExension.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GearSystemExtend
+{
+    public static class StringExension
+    {
+        /// <summary>
+        /// 判断字符串是否为空
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+
+        /// <summary>
+        /// 判断字符串是否为整数
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsNumber(this string str)
+        {
+            return Regex.IsMatch(str, @"^\d+$");
+        }
+
+        /// <summary>
+        /// 判断字符串是否全为汉字
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsSimpleChinese(this string str)
+        {
+            return Regex.IsMatch(str, @"^[\u4e00-\u9fa5]+$");
+        }
+    }
+}
